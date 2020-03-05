6e5097cfc4bae393bd9af146c3360c33623c00c2
gywgithub <qingyi_w@outlook.com>
2020-03-05 21:29:52 +0800

    clear vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue-quill-drag-drop/'
-    : '/',
-  outputDir: 'docs'
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/vue-quill-drag-drop/'
+  //   : '/',
+  // outputDir: 'docs'
 }
