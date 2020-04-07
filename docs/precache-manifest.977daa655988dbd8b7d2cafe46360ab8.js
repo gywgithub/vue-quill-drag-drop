e997386223be3b98602d6ea91306481fb7c9c358
gywgithub <qingyi_w@outlook.com>
2020-04-07 14:58:08 +0800

    refactor(docs): update docs

---
+++ docs/precache-manifest.977daa655988dbd8b7d2cafe46360ab8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d163d082926311487a59",
+    "url": "/vue-quill-drag-drop/css/app.805f821a.css"
+  },
+  {
+    "revision": "d163d082926311487a59",
+    "url": "/vue-quill-drag-drop/js/app.ef49a04a.js"
+  },
+  {
+    "revision": "93176c0d2da5a409a269",
+    "url": "/vue-quill-drag-drop/css/chunk-vendors.7c398ddf.css"
+  },
+  {
+    "revision": "93176c0d2da5a409a269",
+    "url": "/vue-quill-drag-drop/js/chunk-vendors.5b4b0f8d.js"
+  },
+  {
+    "revision": "2cda4fd20527f5ff2cc7a7ad6c0c0345",
+    "url": "/vue-quill-drag-drop/img/1.2cda4fd2.jpg"
+  },
+  {
+    "revision": "039c27b83f92a25ddb575dd89a6cbf24",
+    "url": "/vue-quill-drag-drop/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-quill-drag-drop/robots.txt"
+  }
+];
